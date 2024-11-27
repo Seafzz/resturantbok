7e3f71bd3995a51f9b8e26a697ba275ebbf68aa6
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-11-27 02:44:55 +0000

    added else statement

--- restaurant_booking/booking/views.py
+++ restaurant_booking/booking/views.py
@@ -3,9 +3,12 @@ from .forms import ReservationForm
 
 def book_reservation(request):
     if request.method == 'POST':
-        from = ReservationForm(request.POST)
+        form = ReservationForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('reservation_success')
+        
         else:
+            
             form = ReservationForm()
+        return render(request, 'booking/book_reservation.html', {'form': form})
