c9616ac3dde1d126a5eb4191734778d95ce79972
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-11-27 02:49:21 +0000

    added view_reservation

--- restaurant_booking/booking/views.py
+++ restaurant_booking/booking/views.py
@@ -3,7 +3,7 @@ from .forms import ReservationForm
 from .models import Reservation
 from django.contrib.auth.decorators import login_required
 
-
+#Create reservation
 def book_reservation(request):
     if request.method == 'POST':
         form = ReservationForm(request.POST)
@@ -15,3 +15,10 @@ def book_reservation(request):
             
             form = ReservationForm()
         return render(request, 'booking/book_reservation.html', {'form': form})
+
+
+#View Reservation
+@login_required
+def view_reservation(request):
+    reservations = Reservation.objects.filter(user=request.user)
+    return render(request, 'booking/view_reservations.html', {'reservations': reservations})
