07a82c4611a0f0f5d12d2e8521c5d9c7966611e7
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-11-27 20:12:24 +0000

    updated the url patterns

--- booking/urls.py
+++ booking/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('book/', views.book_reservation, name='book_reservation'),
-    path('view/', views.view_reservations, name='view_reservations'),
+    path('view/', views.view_reservation, name='view_reservations'),  # Changed here
     path('update/<int:reservation_id>/', views.update_reservation, name='update_reservation'),
     path('cancel/<int:reservation_id>/', views.cancel_reservation, name='cancel_reservation'),
     path('reservation_success/', views.reservation_success, name='reservation_success'),
-    ]
+]
