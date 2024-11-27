bf6413ab2d10cde81e00d1d5a1087506493405c0
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-11-27 02:47:20 +0000

    added import reservation and import login required

--- restaurant_booking/booking/views.py
+++ restaurant_booking/booking/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from .forms import ReservationForm
+from .models import Reservation
+from django.contrib.auth.decorators import login_required
+
 
 def book_reservation(request):
     if request.method == 'POST':
