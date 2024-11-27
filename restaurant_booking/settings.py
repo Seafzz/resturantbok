06a289f0aa823ad628b7b290cc7c3cf0f44c9891
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-11-27 03:17:39 +0000

    update: updated the code to look and the the url from .env

--- restaurant_booking/settings.py
+++ restaurant_booking/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'restaurant_booking.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=env('DATABASE_URL'))
+    'default': env.db(),
 }
 
 
